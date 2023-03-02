79181a6316e6ec3cfd4828f6d38732d51e29b1d6
carias21 <cariaslopez11@gmail.com>
2023-03-02 17:44:28 -0600

    cambios plantilla toastr
    se agrega la extension mensake toast
    toastr

--- vistas/assets/dist/js/plantilla.js
+++ vistas/assets/dist/js/plantilla.js
@@ -1,8 +1,8 @@
-$(function() {
+$(function () {
 	validateLoginUsuarioBS4();
 })
 
-function fncSweetAlert(type, text, url){
+function fncSweetAlert(type, text, url) {
 
 	switch (type) {
 
@@ -12,96 +12,210 @@ function fncSweetAlert(type, text, url){
 
 		case "error":
 
-		if(url == null){
+			if (url == null) {
 
-		  	Swal.fire({
-	            icon: 'error',
-	            title: 'Error',
-	            text: text
-	        }) 
+				Swal.fire({
+					icon: 'error',
+					title: 'Error',
+					text: text
+				})
 
-	    }else{
+			} else {
 
-	    	Swal.fire({
-	            icon: 'error',
-	            title: 'Error',
-	            text: text
-	        }).then((result) => {
+				Swal.fire({
+					icon: 'error',
+					title: 'Error',
+					text: text
+				}).then((result) => {
 
-    	 		if (result.value) { 
+					if (result.value) {
 
-    	 			window.open(url, "_top");
+						window.open(url, "_top");
 
-    	 		}
+					}
 
-	        }) 
+				})
 
-	    }
+			}
 
-        break;
+			break;
 
-        /*=============================================
+		/*=============================================
 		Cuando es correcto
 		=============================================*/
 
 		case "success":
 
-		if(url == null){
+			if (url == null) {
 
-		  	Swal.fire({
-	            icon: 'success',
-	            title: 'Success',
-	            text: text,
-				allowOutsideClick: false,
-    			allowEscapeKey: false
-	        }) 
+				Swal.fire({
+					icon: 'success',
+					title: 'Success',
+					text: text,
+					allowOutsideClick: false,
+					allowEscapeKey: false
+				})
 
-	    }else{
+			} else {
 
-	    	Swal.fire({
-	            icon: 'success',
-	            title: 'Confirmación',
-	            text: text,
-				allowOutsideClick: false,
-    			allowEscapeKey: false
-	        }).then((result) => {
+				Swal.fire({
+					icon: 'success',
+					title: 'Confirmación',
+					text: text,
+					allowOutsideClick: false,
+					allowEscapeKey: false
+				}).then((result) => {
 
-    	 		if (result.value) { 
+					if (result.value) {
 
-    	 			window.open(url, "_top");
+						window.open(url, "_top");
 
-    	 		}
+					}
 
-	        }) 
+				})
 
-	    }
+			}
 
-        break;
+			break;
 
-        /*=============================================
+		/*=============================================
+	   Cuando es alerta
+	   =============================================*/
+
+		case "warning":
+
+			if (url == null) {
+
+				Swal.fire({
+					icon: 'warning',
+					title: 'warning',
+					text: text,
+					allowOutsideClick: false,
+					allowEscapeKey: false
+				})
+
+			} else {
+
+				Swal.fire({
+					icon: 'warning',
+					title: 'alert',
+					text: text,
+					allowOutsideClick: false,
+					allowEscapeKey: false
+				}).then((result) => {
+
+					if (result.value) {
+
+						window.open(url, "_top");
+
+					}
+
+				})
+
+			}
+
+			break;
+
+		/*=============================================
+	   Cuando es informacion
+	   =============================================*/
+
+		case "info":
+
+			if (url == null) {
+
+				Swal.fire({
+					icon: 'info',
+					title: 'info',
+					text: text,
+					allowOutsideClick: false,
+					allowEscapeKey: false
+				})
+
+			} else {
+
+				Swal.fire({
+					icon: 'info',
+					title: 'info',
+					text: text,
+					allowOutsideClick: false,
+					allowEscapeKey: false
+				}).then((result) => {
+
+					if (result.value) {
+
+						window.open(url, "_top");
+
+					}
+
+				})
+
+			}
+
+			break;
+
+		/*=============================================
+   Cuando es informacion
+   =============================================*/
+
+		case "info2":
+
+			if (url == null) {
+
+				Swal.fire({
+					icon: 'info2',
+					title: 'info2',
+					text: text,
+					allowOutsideClick: false,
+					allowEscapeKey: false
+				})
+
+			} else {
+
+				Swal.fire({
+					icon: 'info2',
+					title: 'info2',
+					text: text,
+					allowOutsideClick: false,
+					allowEscapeKey: false
+				}).then((result) => {
+
+					if (result.value) {
+
+						window.open(url, "_top");
+
+					}
+
+				})
+
+			}
+
+			break;
+
+		/*=============================================
 		Cuando estamos precargando
 		=============================================*/
 
 		case "loading":
 
-		  Swal.fire({
-            allowOutsideClick: false,
-            icon: 'info',
-            text:text
-          })
-          Swal.showLoading()
+			Swal.fire({
+				allowOutsideClick: false,
+				icon: 'info',
+				text: text
+			})
+			Swal.showLoading()
 
-        break;  
+			break;
 
-        /*=============================================
+		/*=============================================
 		Cuando necesitamos cerrar la alerta suave
 		=============================================*/
 
 		case "close":
 
-		 	Swal.close()
-		 	
-		break;
+			Swal.close()
+
+			break;
 
 
 
@@ -109,25 +223,57 @@ function fncSweetAlert(type, text, url){
 
 }
 
-function validateLoginUsuarioBS4(){
+function validateLoginUsuarioBS4() {
 
-	(function() {
-	  'use strict';
-	  window.addEventListener('load', function() {
-		// Get the forms we want to add validation styles to
-		var forms = document.getElementsByClassName('needs-validation-login');
-		// Loop over them and prevent submission
-		var validation = Array.prototype.filter.call(forms, function(form) {
-		  form.addEventListener('submit', function(event) {
-			if (form.checkValidity() === false) {
-			  event.preventDefault();
-			  event.stopPropagation();
-			}
-			form.classList.add('was-validated');
-									 
-		  }, false);
-		});
-	  }, false);
+	(function () {
+		'use strict';
+		window.addEventListener('load', function () {
+			// Get the forms we want to add validation styles to
+			var forms = document.getElementsByClassName('needs-validation-login');
+			// Loop over them and prevent submission
+			var validation = Array.prototype.filter.call(forms, function (form) {
+				form.addEventListener('submit', function (event) {
+					if (form.checkValidity() === false) {
+						event.preventDefault();
+						event.stopPropagation();
+					}
+					form.classList.add('was-validated');
+
+				}, false);
+			});
+		}, false);
 	})();
+
+}
+
+function mensajeToast(tipo_mensaje, mensaje) {
+
+	toastr.options.progressBar = true;
+	toastr.options.timeOut = 3000; // How long the toast will display without user interaction
+	toastr.options.extendedTimeOut = 3000;
+	toastr.options.rtl = true;
+	toastr.options.preventDuplicates = true;
+
+
+	if (tipo_mensaje == 'error') {
+		toastr.error(mensaje);
+	}
+
+	if (tipo_mensaje == 'success') {
+		toastr.success(mensaje);
+	}
+
+	if (tipo_mensaje == 'info') {
+		toastr.info(mensaje);
+	}
+
+	if (tipo_mensaje == 'warning') {
+		toastr.warning(mensaje);
+	}
+
+	if (tipo_mensaje == 'info2') {
+		toastr.warning(mensaje);
+	}
+
 
 }
