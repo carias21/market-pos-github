ff7fefb6ba1044a7944e9768c7aa338dd0d11dd6
carias21 <cariaslopez11@gmail.com>
2023-03-02 17:45:04 -0600

    se agrega la extension toatr
    se agrega la extension toatr

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -55,8 +55,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 
-    <!-- SweetAlert2 -->
+    <!-- SweetAlert2 TOASTR-->
     <link rel="stylesheet" href="vistas/assets/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 
     <!-- Jquery CSS -->
     <link rel="stylesheet" href="vistas/assets/plugins/jquery-ui/css/jquery-ui.css">
@@ -109,7 +110,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
     <!-- SweetAlert2 -->
     <script src="vistas/assets/plugins/sweetalert2/sweetalert2.min.js"></script>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
     <!-- jquery UI -->
     <script src="vistas/assets/plugins/jquery-ui/js/jquery-ui.js"></script>
 
