d3b6b45f1b6fc832c4f4a93c702c494f66bb4f9a
carias21 <cariaslopez11@gmail.com>
2023-11-28 17:55:37 -0600

    cambios 28-11-23
    catalogo

--- vistas/catalogo/index.php
+++ vistas/catalogo/index.php
@@ -19,7 +19,7 @@ $pagina_actual = isset($_GET['pagina']) ? $_GET['pagina'] : 1; // Página actual
 $inicio = ($pagina_actual - 1) * $resultados_por_pagina; // Índice de inicio para la consulta SQL
 
 // Consulta para obtener los productos del inventario con paginación
-$sql = "SELECT codigo_producto, descripcion_producto, foto, stock_producto FROM productos LIMIT $inicio, $resultados_por_pagina";
+$sql = "SELECT codigo_producto, descripcion_producto, foto, stock_producto FROM productos  ORDER BY RAND() LIMIT $inicio, $resultados_por_pagina";
 $result = $conn->query($sql);
 
 // Consulta para contar el número total de productos
@@ -30,9 +30,17 @@ $total_productos = $result_contar->fetch_assoc()['total'];
 // Calcular el número total de páginas
 $total_paginas = ceil($total_productos / $resultados_por_pagina);
 
+$productos = [];
+while ($row = $result->fetch_assoc()) {
+    $productos[] = $row;
+}
+
 // Cerrar la conexión a la base de datos
 $conn->close();
 ?>
+
+
+
 
 <!DOCTYPE html>
 <html lang="es">
@@ -40,7 +48,7 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Catálogo</title>
+    <title>CATALOGO</title>
     <!-- Enlace a Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
@@ -52,35 +60,70 @@ $conn->close();
 
     ?>
 
-<div class="container">
-        <h1 class="text-center">Catálogo de Productos</h1>
+    <div class="container" style="background-color: #f0f0f0;">
+        <div class="row justify-content-center ">
+            <div id="carouselExample" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <?php
+                    $activeClass = 'active'; // Para la primera imagen
+
+                    foreach ($productos as $producto) {
+                    ?>
+                        <div class="carousel-item <?= $activeClass ?>">
+                            <img src="../assets/imagenes/<?= $producto['foto'] ?>" class="d-block mx-auto img-thumbnail" alt="<?= $producto['descripcion_producto'] ?>" style="max-width: 300px; max-height: 200px;">
+
+                        </div>
+                    <?php
+                        $activeClass = ''; // Desactivar la clase 'active' después de la primera imagen
+                    }
+                    ?>
+                </div>
+
+                <!-- Botones de Navegación -->
+                <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExample" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <br>
+    <div class="container">
+    <div class="card card-info text-center border-info border-3">
+    <h1 class="text-center">PRODUCTOS</h1>
+</div>
+
+
+        <br>
 
         <div class="row">
-            <?php while ($row = $result->fetch_assoc()) : ?>
+            <?php foreach ($productos as $producto) : ?>
                 <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
                     <div class="card card-info text-center">
-                        <img src="../assets/imagenes/<?= $row['foto'] ?>" class="card-img-top mx-auto img-fluid rounded shadow img-thumbnail" alt="<?= $row['descripcion_producto'] ?>" style="max-height: 250px; max-width: 250px; transition: transform 0.3s ease;" onmouseover="this.style.transform='scale(1.3)'" onmouseout="this.style.transform='scale(1)'">
+                        <img src="../assets/imagenes/<?= $producto['foto'] ?>" class="card-img-top mx-auto img-fluid rounded shadow img-thumbnail" alt="<?= $producto['descripcion_producto'] ?>" style="max-height: 150px; max-width: 150px; transition: transform 0.3s ease;" onmouseover="this.style.transform='scale(1.3)'" onmouseout="this.style.transform='scale(1)'">
                         <div class="card-body">
-                            <h5 class="card-title"><?= $row['descripcion_producto'] ?></h5>
+                            <h5 class="card-title"><?= $producto['descripcion_producto'] ?></h5>
 
-                            <?php if ($row['stock_producto'] == 0) : ?>
+                            <?php if ($producto['stock_producto'] == 0) : ?>
                                 <p><span class="text-danger">&#10008;<strong>Agotado</strong> <span style="font-weight:bold;color:red;"></span></span></p>
-                             
                             <?php else : ?>
                                 <p><span class="text-success">&#10004; <strong>Disponible</strong></span></p>
-                          
                             <?php endif; ?>
 
-
-                            <a href="detalle_producto.php?codigo=<?= $row['codigo_producto'] ?>" class="btn btn-info">Ver detalles</a>
+                            <a href="detalle_producto.php?codigo=<?= $producto['codigo_producto'] ?>" class="btn btn-info">Ver detalles</a>
                         </div>
-
                     </div>
                 </div>
-            <?php endwhile; ?>
+            <?php endforeach; ?>
         </div>
-
-
         <!-- Paginación -->
         <nav aria-label="Page navigation">
             <ul class="pagination justify-content-center">
@@ -94,7 +137,7 @@ $conn->close();
     </div>
 
     <!-- Enlace a Bootstrap JS y Popper.js -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
--- vistas/catalogo/modelos/catalogo.modelo.php
+++ vistas/catalogo/modelos/catalogo.modelo.php
@@ -33,16 +33,16 @@ class CatalogoModelo
     }
 
 
-    
+
     static public function mdlListarNombreProductos()
     {
 
 
-        $stmt = Conexion::conectar()->prepare("SELECT  codigo_producto,
+        $stmt = Conexion::conectar()->prepare("SELECT  codigo_producto, c.nombre_categoria,
                                             descripcion_producto,
-                        
                                             foto
-                                        FROM productos");
+                                        FROM productos
+                                        inner join categorias c on c.id_categoria = id_categoria_producto");
 
 
 
@@ -57,26 +57,21 @@ class CatalogoModelo
 
             $descripcion_producto = $producto["descripcion_producto"];
             $data["id"] = $producto["codigo_producto"];
+            $categoria = $producto["nombre_categoria"];
+            $data["value"] = $categoria;
             $data["value"] = $descripcion_producto;
-            $data["label"] = '<a href="javascript:void(0);" class="d-flex producto-link" style="width:100% !important; text-decoration: none; color: #333;">
-                            <img src="../assets/imagenes/' . $producto['foto'] . '" width="100" height="100" style="border-radius: 2px; object-fit: cover;"/>
-                            <div class="d-flex ml-4 flex-column">
-                                <div style="font-size: 25px; text-align: center;"><strong>Producto:</strong> ' . $descripcion_producto . '</div>
-                            </div>
-                        </a>';
-    
+            $data["label"] = '<a href="javascript:void(0);" class="d-flex producto-link" style="width:100%; text-decoration: none; border: 5px solid #ddd; border-radius: 50px; padding: 10px; transition: box-shadow 0.3s;">
+            <img src="../assets/imagenes/' . $producto['foto'] . '" width="100" height="100" style="border-radius: 10%; object-fit: cover; border: 3px solid #fff; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);"/>
+            <div class="d-flex flex-column ml-4">
+            <span style="font-size: 20px; "><strong>Categoría:</strong> ' . $categoria . '</span>
+            <span style="font-size: 20px;"><strong>Producto:</strong> ' . $descripcion_producto . '</span>
+            </div>
+            </a>';
 
-            
+
 
             array_push($productData, $data);
         }
         return $productData;
     }
-
-
-
-
-
-   
-
 }
